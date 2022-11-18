401041d2ec4ac85bd1f847a6a6f2a9562073bc43
Auner Medina <amedina1@godaddy.com>
2022-11-17 18:44:21 -0600

    add config files for heroku deployment

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,18 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "0r4KL314",
-  DB: "auth-api",
+  HOST: "ec2-54-163-34-107.compute-1.amazonaws.com",
+  USER: "sgbvyembcptlfk",
+  PASSWORD: "cf6545aa7edebfcf3c6af16367624adf37b58838bf264bd84a52307ec65c8b27",
+  DB: "d47pamsm14trhq",
   dialect: "postgres",
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
     idle: 10000
+  },
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 };
--- app/models/index.js
+++ app/models/index.js
@@ -8,6 +8,7 @@ const sequelize = new Sequelize(
   {
     host: config.HOST,
     dialect: config.dialect,
+    dialectOptions: config.dialectOptions,
     operatorsAliases: false,
 
     pool: {
